06443b31701f3594e21c2fc0fe357d08fb0976f8
Tobirama <grigoroscuta.sergiu@yahoo.com>
2022-12-14 21:15:54 +0200

    blank

---
+++ src/components/MadeBy.js
@@ -0,0 +1,11 @@
+export default function MadeBy() {
+  return (
+    <div className="attribution">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Your Name Here</a>.
+    </div>
+  );
+}
